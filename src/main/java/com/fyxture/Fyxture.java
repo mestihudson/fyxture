dbf5e8a54e10133645171366d4f21a3edf30ab59
mestihudson <mestihudson@gmail.com>
2014-11-26 17:57:54 -0200

    clear count insert

--- src/main/java/com/fyxture/Fyxture.java
+++ src/main/java/com/fyxture/Fyxture.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.DriverManager;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
@@ -17,30 +20,104 @@ public class Fyxture {
   private static Logger logger = Logger.getLogger(Fyxture.class);
   private static Map<String, Object> map = new LinkedHashMap<String, Object>();
   private static Fyxture instance;
-  private static Connection connection;
+  private static Connection connection = null;
+  private static Statement statement = null;
   private static String driver;
   private static String url;
   private static String user;
   private static String password;
   private Object entity = null;
   private String entityname = null;
-private String entityfilter;
+  private String entityfilter;
 
-  private Fyxture(String driver, String url, String user, String password) {
+  private static final String DELETE = "DELETE FROM %s;";
+  private static final String SEQUENCE_DROP = "DROP SEQUENCE %s;";
+  private static final String SEQUENCE_CREATE = "CREATE SEQUENCE %s START WITH 1 BELONGS_TO_TABLE;";
+  private static final String SEQUENCE_ALTER = "ALTER SEQUENCE %s RESTART WITH 1;";
+  private static final String COUNT = "SELECT COUNT(1) FROM %s;";
+  private static final String INSERT = "INSERT INTO %s (%s) VALUES (%s);";
+
+  public static void clear() throws Throwable {
+    init();
+    Map entities = m(get("init.cfg", "entities"));
+    String tables = "";
+    String drop_sequences = "";
+    String create_sequences = "";
+    String alter_sequences = "";
+    for(Object key : entities.keySet()){
+      tables = tables.concat(String.format(DELETE, key));
+      try{
+        String sequence = s(get("init.cfg", String.format("entities.%s.sequence.name", key)));
+        alter_sequences = alter_sequences.concat(String.format(SEQUENCE_ALTER, sequence));
+      }catch(Throwable t){}
+    }
+    String command = tables.concat(alter_sequences);
+    logger.info(command);
+    statement.executeUpdate(command);
+  }
+
+  public static Integer count(String entity) throws Throwable {
+    init();
+    String command = String.format(COUNT, entity);
+    ResultSet rs = statement.executeQuery(command);
+    rs.next();
+    return rs.getInt(1);
+  }
+
+  public static void insert(String entity) throws Throwable {
+    init();
+    String suffix = s(get("init.cfg", "entity.suffix"));
+    String default_descriptor = s(get("init.cfg", "entity.default.descriptor"));
+    String entitydes = String.format("%s.%s", entity, suffix);
+
+    logger.info(entitydes);
+    logger.info(default_descriptor);
+    Map c = m(get(entitydes, default_descriptor));
+    logger.info(c);
+    String cols = "";
+    String vals = "";
+    for(Object key : c.keySet()){
+      cols = cols.concat((cols.equals("") ? "" : ", ") + key);
+      Object v = c.get(key);
+      v = v instanceof String ? "'" + v + "'" : v;
+      vals = vals.concat((vals.equals("") ? "" : ", ") + v);
+    }
+    String command = String.format(INSERT, entity, cols, vals);
+    logger.info(command);
+    statement.executeUpdate(command);
+
+
+/*
+    String r = "";
+    for(Object k : m(o).keySet()){
+      Object v = m(o).get(k);
+      v = v instanceof String ? "'" + v + "'" : v;
+      String c = s(get("init.cfg", "entities." + entityname + ".sequence.column"));
+      String n = s(get("init.cfg", "entities." + entityname + ".sequence.name"));
+      v = v == null ? (c.equals(k.toString()) ? n.concat(".nextval") : v) : v;
+      r += r.length() == 0 ? v : ",".concat(v.toString());
+    }
+    return r;
+    */
+  }
+
+  private Fyxture(String driver, String url, String user, String password) throws Throwable {
     this.driver = driver;
     this.url = url;
     this.user = user;
     this.password = password;
+    Class.forName(this.driver).newInstance();
+    this.connection = DriverManager.getConnection(this.url, this.user, this.password);
+    this.statement = this.connection.createStatement();
   }
 
   public static Fyxture init() throws Throwable {
-	//logger.info(m(get("init.cfg")));
     if(instance == null){
       instance = new Fyxture(
-        s(get("init.cfg","dev.driver")), 
-        s(get("init.cfg","dev.url")),
-        s(get("init.cfg","dev.user")),
-        s(get("init.cfg","dev.password"))
+        s(get("init.cfg","ds.dev.driver")), 
+        s(get("init.cfg","ds.dev.url")),
+        s(get("init.cfg","ds.dev.user")),
+        s(get("init.cfg","ds.dev.password"))
       );
     }
     return instance;
@@ -70,7 +147,7 @@ private String entityfilter;
   }
 
   private static Object seg(Object o, String... p) {
-	Object no = o instanceof Map ? m(o).get(p[0]) : o;
+    Object no = o instanceof Map ? m(o).get(p[0]) : o;
     try{
       return seg(no, extract(p, 0));
     }catch(Throwable t){
@@ -97,40 +174,40 @@ private String entityfilter;
   }
 
   public Fyxture entity(String name, String filter) throws Throwable {
-	entityname = name;
-	entityfilter = filter;
-	entity = m(get(name.concat(".ent"))).get(filter);
-	return instance;
+  	entityname = name;
+  	entityfilter = filter;
+  	entity = m(get(name.concat(".ent"))).get(filter);
+  	return instance;
   }
 
   public Fyxture entity(String name) throws Throwable {
-	return entity(name, "default");
+	 return entity(name, "default");
   }
 
-  public Fyxture insert() throws Throwable {
-	String command = String.format("INSERT INTO %s (%s) VALUES (%s);", entityname, columns(), values());
-	logger.info(command);
-	return instance;
+  public Fyxture insert1() throws Throwable {
+  	String command = String.format("INSERT INTO %s (%s) VALUES (%s);", entityname, columns(), values());
+  	logger.info(command);
+  	return instance;
   }
 
   private String values() throws Throwable {
-	String r = "";
-	for(Object k : m(entity).keySet()){
-		Object v = m(entity).get(k);
-		v = v instanceof String ? "'" + v + "'" : v;
-		String c = s(get("init.cfg", "entities." + entityname + ".sequence.column"));
-		String n = s(get("init.cfg", "entities." + entityname + ".sequence.name"));
-		v = v == null ? (c.equals(k.toString()) ? n.concat(".nextval") : v) : v;
-		r += r.length() == 0 ? v : ",".concat(v.toString());
-	}
-	return r;
+  	String r = "";
+  	for(Object k : m(entity).keySet()){
+  		Object v = m(entity).get(k);
+  		v = v instanceof String ? "'" + v + "'" : v;
+  		String c = s(get("init.cfg", "entities." + entityname + ".sequence.column"));
+  		String n = s(get("init.cfg", "entities." + entityname + ".sequence.name"));
+  		v = v == null ? (c.equals(k.toString()) ? n.concat(".nextval") : v) : v;
+  		r += r.length() == 0 ? v : ",".concat(v.toString());
+  	}
+  	return r;
   }
 
   private String columns() {
-	String r = "";
-	for(Object k : m(entity).keySet()){
-		r += r.length() == 0 ? k : ",".concat(k.toString());
-	}
-	return r;
+  	String r = "";
+  	for(Object k : m(entity).keySet()){
+  		r += r.length() == 0 ? k : ",".concat(k.toString());
+  	}
+  	return r;
   }
 }
--- src/test/java/com/fyxture/FyxtureTest.java
+++ src/test/java/com/fyxture/FyxtureTest.java
@@ -4,14 +4,72 @@ import org.junit.Test;
 import org.junit.Assert;
 import org.apache.log4j.Logger;
 import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.After;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.DriverManager;
+import org.flywaydb.core.Flyway;
 
 public class FyxtureTest {
   private static Logger logger = Logger.getLogger(FyxtureTest.class);
+  private Connection connection = null;
+  private Statement statement = null;
+  private static Flyway flyway = null;
 
-  @Test public void init() throws Throwable {
-	//logger.info(Fyxture.init().entity("pessoa", "id_pessoa:1"));
-	logger.info(Fyxture.init().entity("pessoa", "id_pessoa:1").insert());
-	logger.info(Fyxture.init().entity("pessoa", "id_pessoa:2").insert());
-    //Assert.assertThat(Fyxture.init().entity("pessoa"), Matchers.not(Matchers.equalTo(null)));
+  @Before public void init() throws Throwable {
+    reset();
+    Class.forName("org.h2.Driver").newInstance();
+    connection = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/blog", "sa", null);
+    statement = connection.createStatement();
+  }
+
+  private void reset() throws Throwable {
+    if(flyway == null){
+      flyway = new Flyway();
+      flyway.setDataSource("jdbc:h2:tcp://localhost/~/blog", "sa", null);
+    }
+    flyway.clean();
+    flyway.migrate();
+  }
+
+  @After public void close() throws Throwable {
+    try{
+      statement.close();
+      connection.close();
+    }catch(Throwable t){
+      statement = null;
+      connection = null;
+    }
+  }
+
+  @Test public void clear() throws Throwable {
+    create();
+    Fyxture.clear();
+    assertCleaned();
+  }
+
+  @Test public void count() throws Throwable {
+    Assert.assertThat(Fyxture.count("livro"), Matchers.equalTo(0));
+    create();
+    Assert.assertThat(Fyxture.count("livro"), Matchers.equalTo(1));
+  }
+
+  @Test public void insert() throws Throwable {
+    Fyxture.insert("livro");
+    assertHave();
+  }
+
+  private void create() throws Throwable {
+    statement.executeUpdate("INSERT INTO LIVRO (ID, VERSION, ANO, TITULO) VALUES (1, 0, 1885, 'Dom Casmurro')");
+  }
+
+  private void assertCleaned() throws Throwable {
+    Assert.assertThat(statement.executeQuery("SELECT * FROM LIVRO").next(), Matchers.equalTo(false));
+  }
+
+  private void assertHave() throws Throwable {
+    Assert.assertThat(statement.executeQuery("SELECT * FROM LIVRO").next(), Matchers.equalTo(true));
   }
 }
