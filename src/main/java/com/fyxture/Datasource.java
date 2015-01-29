fe776139e3796e6365f465cd4098f2e7a2c8be60
mestihudson <mestihudson@gmail.com>
2015-01-29 16:51:07 -0200

    inicio da groovy-impl

--- src/main/java/com/fyxture/Datasource.java
+++
@@ -1,6 +0,0 @@
-package com.fyxture;
-
-public enum Datasource {
-  H2,
-  ORACLE;
-}
--- src/main/java/com/fyxture/FyxtureVerifyFail.java
+++
@@ -1,7 +0,0 @@
-package com.fyxture;
-
-public class FyxtureVerifyFail extends Exception {
-  public FyxtureVerifyFail(String message) {
-	super(message);
-  }
-}
