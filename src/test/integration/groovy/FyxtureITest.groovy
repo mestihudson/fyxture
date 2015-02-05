8cf80a70f1d987e0ab43f3ec9336c292a1292db6
mestihudson <mestihudson@gmail.com>
2015-02-05 17:26:09 -0200

    qualquer coisa

--- src/test/integration/groovy/FyxtureITest.groovy
+++ src/test/integration/groovy/FyxtureITest.groovy
@@ -1,18 +1,30 @@
 import org.junit.Test
+import org.junit.After
+import org.junit.Before
 import org.junit.Ignore
 
 import org.yaml.snakeyaml.Yaml
 
 class FyxtureITest {
-  @Test @Ignore void "test integração"() {
-    Yaml yaml = new Yaml()
-    String document = """
+  def file
+  def yaml = new Yaml()
+
+  @Before void "criar arquivo de configuração"() {
+    file = new File("config.yml")
+  }
+
+  @After void "eliminar arquivo de configuração"() {
+    file.delete()
+  }
+
+  void config(String content) {
+    file.write content
+  }
+
+  @Test void "test integração"() {
+    config("""
 common:
   datasource: h2
-
-"""
-    new File("config.yml").write(document)
-    //println yaml.dump(yaml.load(document))
-    // Fyxture.clear()
+""")
   }
 }
